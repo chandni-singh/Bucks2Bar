46080150d53dbd7f1f1409fbc006b01095292ae6
Chandni <chandni1141996@gmail.com>
2025-02-17 13:42:07 +0530

    added CORS middleware and object-assign dependency; updated input values in July section of index.html

--- server.js
+++ server.js
@@ -1,44 +1,50 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+// Use CORS middleware
+app.use(cors());
 
 app.use(bodyParser.json({ limit: "10mb" }));
 
 app.post("/send-email", (req, res) => {
   const { email, chartImage } = req.body;
-  console.log("works!!!");
+  console.log("works!!!", { email, chartImage });
 
-  //   const transporter = nodemailer.createTransport({
-  //     service: "gmail",
-  //     auth: {
-  //       user: "your-email@gmail.com",
-  //       pass: "your-email-password",
-  //     },
-  //   });
+  const transporter = nodemailer.createTransport({
+    host: "smtp.resend.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "resend",
+      pass: "re_MJpJu3k5_54yX8xMttavGAYpWSRjeds8X",
+    },
+  });
 
-  //   const mailOptions = {
-  //     from: "your-email@gmail.com",
-  //     to: email,
-  //     subject: "Your Chart Image",
-  //     text: "Please find the attached chart image.",
-  //     attachments: [
-  //       {
-  //         filename: "chart.png",
-  //         content: chartImage.split("base64,")[1],
-  //         encoding: "base64",
-  //       },
-  //     ],
-  //   };
+  const mailOptions = {
+    from: "test@resend.dev",
+    to: email,
+    subject: "Your Chart Image",
+    text: "Please find the attached chart image.",
+    attachments: [
+      {
+        filename: "chart.png",
+        content: chartImage.split("base64,")[1],
+        encoding: "base64",
+      },
+    ],
+  };
 
-  //   transporter.sendMail(mailOptions, (error, info) => {
-  //     if (error) {
-  //       return res.status(500).json({ message: "Error sending email" });
-  //     }
-  //     res.status(200).json({ message: "Email sent successfully" });
-  //   });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).json({ message: "Error sending email" });
+    }
+    res.status(200).json({ message: "Email sent successfully" });
+  });
 });
 
 app.listen(port, () => {
